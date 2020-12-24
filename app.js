9e1a3996944c3423d461165f328689e8e70dc342
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2020-12-25 01:52:26 +0600

    added cors policy

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const cors = require('cors');
 const mongoose = require('mongoose')
 require('dotenv/config')
 
 const app = express()
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
